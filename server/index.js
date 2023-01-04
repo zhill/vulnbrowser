98b94e276ef540f541de90c71241fb41ff215c54
Zach Hill <zhill.dev@gmail.com>
2023-01-03 22:18:41 -0800

    chore: initial setup with yarn v2 and basic app
    Signed-off-by: Zach Hill <zhill.dev@gmail.com>

--- server/index.js
+++ server/index.js
@@ -16,28 +16,33 @@ app.use(express.static(path.resolve(__dirname, '../client/build')));
 // GET /api
 app.get("/api", (req, res) => {
     res.json({ message: "v1" });
+    console.log("Getting api");
 });
 
 // POST /vulnerabilities - create a new vulnerability
 app.post("/vulnerabilities", (req, res) => {
     vulns.append(req.data);
     res.json({status: "success", count: vulns.length});
+    console.log("Adding vuln");
 });
 
 // GET /vulnerabilities - return the full list
 app.get("/vulnerabilities", (req, res) => {
     res.json(vulns);
+    console.log("Sending vulns " + vulns);
 });
 
 // POST /affected_packages
 app.post("/affected_packages", (req, res) => {
     affectedPackages.append(req.data);
     res.json({status: "success", count: affectedPackages.length});
+    console.log("Adding affected package");
 });
 
 
 // Serve up the client
 app.get('*', (req, res) => {
+    console.log("Sending client");
     res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'))
 });
 
